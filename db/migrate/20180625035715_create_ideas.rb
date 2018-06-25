14262f18dc7e557eae8f9356c1b4f3d54ed915b5
nariatsu1202 <atsuki02jojo@gmail.com>
2018-06-25 13:06:43 +0900

    create idea table

--- db/migrate/20180625035715_create_ideas.rb
+++ db/migrate/20180625035715_create_ideas.rb
@@ -1,7 +1,6 @@
 class CreateIdeas < ActiveRecord::Migration[5.1]
   def change
     create_table :ideas do |t|
-      t.references :user, foreign_key: true
       t.text :idea_text, null: false
       t.string :image_url
       t.timestamps
